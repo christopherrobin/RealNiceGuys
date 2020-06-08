f4e0255720fadc718389158df422e0b997f3f603
Christopher Reynolds <christopher.reynolds@angieslist.com>
2020-06-08 17:28:17 -0400

    Final pass at home page and music player + hack the planet component + adding image resources for when the album is released

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import HackThePlanet from './HackThePlanet';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import logo from './sometimesitsdarkbutthatsokay.png';
+// import SpotifyLogo from './spotify.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>REAL NICE GUYS</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div id="music-player">
-          <iframe
-            title="Real Nice Guys Music Player"
-            id="iframe"
-            style={{ border: 0, width: '20em', marginTop: '2em', height: '2em', margin: 'auto' }}
-            src="https://bandcamp.com/EmbeddedPlayer/album=499365377/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1294941296/transparent=true/" seamless>
-            <a href="http://realniceguys.bandcamp.com/album/holding-on-to-this-feeling-single"
-          >
-            Holding on to This Feeling (single) by Real Nice Guys</a>
-          </iframe>
-        </div>
-      </header>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <div id="top-banner">
+                <iframe
+                  id="iframe"
+                  title="Real Nice Guys Music Player"
+                  src="https://bandcamp.com/EmbeddedPlayer/album=499365377/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1294941296/transparent=true/" seamless>
+                  <a href="http://realniceguys.bandcamp.com/album/holding-on-to-this-feeling-single"
+                >
+                  Holding on to This Feeling (single) by Real Nice Guys</a>
+                </iframe>
+              </div>
+              <header className="App-header">
+                <h1>REAL NICE GUYS</h1>
+              </header>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Route>
+            <Route path="/hacktheplanet">
+              <HackThePlanet />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/HackThePlanet.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HackThePlanet = () => {
+  return (
+    <div>
+      <h1 style={{ fontSize: '8em', color: 'white', marginTop: '.2em', lineHeight: '0.8em' }}>HACK THE PLANET</h1>
+      <img
+        alt="Animated GIF from the movie 'Hackers'"
+        src="https://media.giphy.com/media/HAonhTZTaQE24/giphy.gif"
+        style={{ border: 'white solid 1em', maxWidth: '84%' }}
+      />
+      <p style={{ width: '90%',  margin: 'auto', marginTop: '2em', textAlign: 'left', color: 'white' }}>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est.
+      </p>
+    </div>
+  );
+}
+
+export default (HackThePlanet);
