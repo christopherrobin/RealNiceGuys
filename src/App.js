721f0155f7e3fdd048ac389126c8a4e2e58d8910
Christopher Reynolds <christopher.reynolds@angieslist.com>
2020-06-08 20:29:12 -0400

    Bottom media bar implementation

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import logo from './sometimesitsdarkbutthatsokay.png';
-// import SpotifyLogo from './spotify.svg';
+import RNGLogo from './sometimesitsdarkbutthatsokay.png';
+import SpotifyLogo from './spotify.svg';
+import AmazonLogo from './amazon.png';
+import ITunesLogo from './itunes.svg';
+import InstagramLogo from './instagram.png';
+import YoutubeLogo from './youtube.svg';
 import './App.css';
 
 const App = () => {
@@ -15,20 +19,27 @@ const App = () => {
         <Router>
           <Switch>
             <Route exact path="/">
-              <div id="top-banner">
-                <iframe
-                  id="iframe"
-                  title="Real Nice Guys Music Player"
-                  src="https://bandcamp.com/EmbeddedPlayer/album=499365377/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1294941296/transparent=true/" seamless>
-                  <a href="http://realniceguys.bandcamp.com/album/holding-on-to-this-feeling-single"
-                >
-                  Holding on to This Feeling (single) by Real Nice Guys</a>
-                </iframe>
-              </div>
               <header className="App-header">
                 <h1>REAL NICE GUYS</h1>
               </header>
-              <img src={logo} className="App-logo" alt="logo" />
+              <img src={RNGLogo} className="App-logo" alt="Real Nice Guys Album Art" />
+              <div id="links">
+                <div id="top-banner">
+                  <iframe
+                    id="iframe"
+                    title="Real Nice Guys Music Player"
+                    src="https://bandcamp.com/EmbeddedPlayer/album=499365377/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1294941296/transparent=true/" seamless>
+                    <a href="http://realniceguys.bandcamp.com/album/holding-on-to-this-feeling-single"
+                  >
+                    Holding on to This Feeling (single) by Real Nice Guys</a>
+                  </iframe>
+                </div>
+                <img src={SpotifyLogo} alt="Listen to Real Nice Guys on Spotify" className="logo-sm coming-soon" style={{ marginLeft: 0 }} />
+                <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UC_9P0tKrLf1x_qFQ_KZbqLw"><img src={YoutubeLogo} alt="Real Nice Guys on YouTube" className="logo-bg" /></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/real_nice_guys/"><img src={InstagramLogo} alt="Real Nice Guys on Instagram" className="logo-sm" /></a>
+                <img src={ITunesLogo} alt="Listen to Real Nice Guys on iTunes" className="logo-sm coming-soon" />
+                <img src={AmazonLogo} alt="Listen to Real Nice Guys on Amazon" className="logo-bg coming-soon" />
+              </div>
             </Route>
             <Route path="/hacktheplanet">
               <HackThePlanet />
