a4b4c3a0affb63186b0796063df078941097125e
Christopher Reynolds <christopher.reynolds@angieslist.com>
2020-08-18 16:54:43 -0400

    Fix full screen issue

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import FullHeight from "react-full-height";
 import RNGLogo from './sometimesitsdarkbutthatsokay.png';
 import SpotifyLogo from './spotify.svg';
 import AmazonLogo from './amazonmusic.png';
@@ -24,40 +25,42 @@ const App = () => {
     };
     const onOrOff = state.checkedA ? 'on' : 'off';
   return (
-    <div className="App">
+    <div className="App" id={`blink-${onOrOff}`}>
         <Router>
           <Switch>
             <Route path="/HackThePlanet" component={HackThePlanet}>
             </Route>
             <Route exact path="/">
-              <div className={`blink-${onOrOff}`}>
-                <Killswitch checkedA={state.checkedA} handleChange={handleChange} />
-                <header className="App-header">
-                  <h1>REAL NICE GUYS</h1>
-                </header>
-                <img
-                  src={RNGLogo} className="App-logo"
-                  alt="Real Nice Guys Smiley Face"
-                  title="Real Nice Guys Smiley Face"
-                />
-                <div id="links">
-                  <div id="top-banner">
-                    <iframe
-                      id="iframe"
-                      title="Real Nice Guys Music Player"
-                      src="https://bandcamp.com/EmbeddedPlayer/album=499365377/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1294941296/transparent=true/" seamless>
-                      <a href="http://realniceguys.bandcamp.com/album/holding-on-to-this-feeling-single"
-                    >
-                      Holding on to This Feeling (single) by Real Nice Guys</a>
-                    </iframe>
+              <FullHeight>
+                <div>
+                  <Killswitch checkedA={state.checkedA} handleChange={handleChange} />
+                  <header className="App-header">
+                    <h1>Real Nice Guys</h1>
+                  </header>
+                  <img
+                    src={RNGLogo} className="App-logo"
+                    alt="Real Nice Guys Smiley Face"
+                    title="Real Nice Guys Smiley Face"
+                  />
+                  <div id="links">
+                    <div id="top-banner">
+                      <iframe
+                        id="iframe"
+                        title="Real Nice Guys Music Player"
+                        src="https://bandcamp.com/EmbeddedPlayer/album=499365377/size=small/bgcol=ffffff/linkcol=63b2cc/artwork=none/track=1294941296/transparent=true/" seamless>
+                        <a href="http://realniceguys.bandcamp.com/album/holding-on-to-this-feeling-single"
+                      >
+                        Holding on to This Feeling (single) by Real Nice Guys</a>
+                      </iframe>
+                    </div>
+                      <img src={SpotifyLogo} alt="Listen to Real Nice Guys on Spotify" className="logo-sm coming-soon" style={{ marginLeft: 0 }} />
+                      <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UC_9P0tKrLf1x_qFQ_KZbqLw"><img src={YoutubeLogo} alt="Real Nice Guys on YouTube" className="logo-bg" /></a>
+                      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/real_nice_guys/"><img src={InstagramLogo} alt="Real Nice Guys on Instagram" className="logo-sm" /></a>
+                      <img src={ITunesLogo} alt="Listen to Real Nice Guys on iTunes" className="logo-sm coming-soon" />
+                      <img src={AmazonLogo} alt="Listen to Real Nice Guys on Amazon" className="logo-bg coming-soon" />
                   </div>
-                    <img src={SpotifyLogo} alt="Listen to Real Nice Guys on Spotify" className="logo-sm coming-soon" style={{ marginLeft: 0 }} />
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UC_9P0tKrLf1x_qFQ_KZbqLw"><img src={YoutubeLogo} alt="Real Nice Guys on YouTube" className="logo-bg" /></a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/real_nice_guys/"><img src={InstagramLogo} alt="Real Nice Guys on Instagram" className="logo-sm" /></a>
-                    <img src={ITunesLogo} alt="Listen to Real Nice Guys on iTunes" className="logo-sm coming-soon" />
-                    <img src={AmazonLogo} alt="Listen to Real Nice Guys on Amazon" className="logo-bg coming-soon" />
                 </div>
-              </div>
+              </FullHeight>
             </Route>
           </Switch>
         </Router>
