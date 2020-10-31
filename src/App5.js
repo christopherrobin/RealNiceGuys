ee106f407b718ad10c224ee8b816635d4b16c2d0
christopherrobin <christopherrobinreynolds@gmail.com>
2020-10-31 14:18:06 -0400

    switching links to top

--- src/App5.js
+++ src/App5.js
@@ -51,6 +51,15 @@ const App = () => {
                     <Container>
                       <Row>
                         <Col xs={12}><h1 id="header" className="text-center">Real Nice Guys</h1></Col>
+                        <Col xs={12}>
+                          <div className="text-center" style={{margin: '.5em 0 1.5em 0'}}>
+                            <a alt="Real Nice Guys on Instagram" href="https://www.instagram.com/real_nice_guys/" className="social-icon-nomargin" target="_blank" rel="noopener noreferrer"><InstagramIcon fontSize="large" /></a>
+                            <a alt="Real Nice Guys on YouTube" href="https://www.youtube.com/channel/UC_9P0tKrLf1x_qFQ_KZbqLw" className="social-icon" target="_blank" rel="noopener noreferrer"><YouTubeIcon fontSize="large" /></a>
+                            <a alt="Real Nice Guys on Twitter" href="https://twitter.com/Real_Nice_Guys" className="social-icon" target="_blank" rel="noopener noreferrer"><TwitterIcon fontSize="large" /></a>
+                            <a alt="Real Nice Guys on Facebook" href="https://www.facebook.com/RealNiceGuys/" className="social-icon" target="_blank" rel="noopener noreferrer"><FacebookIcon fontSize="large" /></a>
+                            <div style={{marginTop: '1em'}}><Mailto style={{ display: 'block', fontWeight: 'bold' }} className="social-icon-nomargin" email='info@realniceguys.com' /></div>
+                          </div>
+                        </Col>
                         <Col xs={12} className="text-center">
                           <div className="text-center">
                             <iframe
@@ -62,13 +71,6 @@ const App = () => {
                               className="player"
                             >
                           </iframe>
-                          </div>
-                          <div className="text-center">
-                            <div style={{ margin: '0 0 1em 0' }}><Mailto style={{ display: 'block', fontWeight: 'bold'}} className="social-icon-nomargin" email='info@realniceguys.com' /></div>
-                            <a alt="Real Nice Guys on Instagram" href="https://www.instagram.com/real_nice_guys/" className="social-icon-nomargin" target="_blank" rel="noopener noreferrer"><InstagramIcon fontSize="large" /></a>
-                            <a alt="Real Nice Guys on YouTube" href="https://www.youtube.com/channel/UC_9P0tKrLf1x_qFQ_KZbqLw" className="social-icon" target="_blank" rel="noopener noreferrer"><YouTubeIcon fontSize="large" /></a>
-                            <a alt="Real Nice Guys on Twitter" href="https://twitter.com/Real_Nice_Guys" className="social-icon" target="_blank" rel="noopener noreferrer"><TwitterIcon fontSize="large" /></a>
-                            <a alt="Real Nice Guys on Facebook" href="https://www.facebook.com/RealNiceGuys/" className="social-icon" target="_blank" rel="noopener noreferrer"><FacebookIcon fontSize="large" /></a>
                           </div>
                         </Col>
                       </Row>
